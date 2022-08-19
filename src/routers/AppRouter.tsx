c821e5058e4645a64cfdb67ff43401af8a7df14b
Olga <olga@gmail.com>
2022-08-19 20:27:41 +0300

    refactor(routing): rename route enum

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import { routes } from "./routes";
+import { ROUTE } from "./routes";
 import { MainTemplate } from "../components";
 import { Favourits, Home, Movie, NotFound, Settings, SignIn, SignUp, Trends } from "../pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={routes.HOME} element={<MainTemplate />}>
+      <Route path={ROUTE.HOME} element={<MainTemplate />}>
         <Route index element={<Home />} />
-        <Route path={routes.TRENDS} element={<Trends />} />
-        <Route path={routes.FAVOURITS} element={<Favourits />} />
-        <Route path={routes.SETTINGS} element={<Settings />} />
-        <Route path={routes.MOVIE} element={<Movie />} />
-        <Route path={routes.SIGN_IN} element={<SignIn />} />
-        <Route path={routes.SIGN_UP} element={<SignUp />} />
-        <Route path={routes.NOT_FOUND} element={<NotFound />} />
+        <Route path={ROUTE.TRENDS} element={<Trends />} />
+        <Route path={ROUTE.FAVOURITS} element={<Favourits />} />
+        <Route path={ROUTE.SETTINGS} element={<Settings />} />
+        <Route path={ROUTE.MOVIE} element={<Movie />} />
+        <Route path={ROUTE.SIGN_IN} element={<SignIn />} />
+        <Route path={ROUTE.SIGN_UP} element={<SignUp />} />
+        <Route path={ROUTE.NOT_FOUND} element={<NotFound />} />
       </Route>
     </Routes>
   );
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,9 +1,9 @@
-export enum routes {
+export enum ROUTE {
     HOME = "/",
     TRENDS = "trends",
     FAVOURITS = "favourits",
     SETTINGS = "settings",
-    MOVIE = "movie",
+    MOVIE = "movie/:id",
     SIGN_IN = "sign-in",
     SIGN_UP = "sign-up",
     NOT_FOUND = "*"
