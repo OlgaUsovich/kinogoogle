cde7a0739908bbd6bb204a2d50274156e4dc447e
Olga <olga@gmail.com>
2022-08-20 20:56:13 +0300

    feat(api): add getting movies data by api

--- src/services/movieApi.ts
+++ src/services/movieApi.ts
@@ -1,32 +1,66 @@
 import axios from "axios";
 
-export type MovieRequestParams =
-    | 's'
-    | 'type'
-    | 'y'
-    | 'r'
-    | 'page'
-    | 'v';
+export type MovieRequestParams = {
+  apikey?: string;
+  s?: string;
+  type?: string;
+  y?: string;
+  r?: string;
+  page?: string;
+  v?: string;
+  i?: string;
+};
 
 class MovieAPI {
-    private readonly BASE_URL = process.env.BASE_URL_MOVIES_API;
-    private readonly API = axios.create({
-        baseURL: this.BASE_URL,
+  private readonly BASE_URL = "http://www.omdbapi.com/"; //не приходит урл из .env
+  private readonly API = axios.create({
+    baseURL: this.BASE_URL,
+  });
+
+  private readonly DEFALUT_REQUEST_PARAMS: MovieRequestParams = { apikey: "333f98df" }; //не приходит из .env
+
+  public getRandomParam(): string {
+    const searchWords = [
+      "cat",
+      "dog",
+      "butterfly",
+      "car",
+      "bad",
+      "super",
+      "hero",
+      "girl",
+      "boy",
+      "suffer",
+      "murder",
+      "kill",
+      "mistery",
+      "bat",
+      "happy",
+      "hello",
+      "chance",
+      "clever"
+    ];
+
+    return searchWords[Math.floor(Math.random() * searchWords.length)];
+  }
+
+  public async getAll(newParams: MovieRequestParams): Promise<any> {
+    const params = { ...this.DEFALUT_REQUEST_PARAMS, ...newParams, s: this.getRandomParam() };
+
+    const { data } = await this.API.get("", {
+      params,
     });
 
-    private readonly DEFALUT_REQUEST_PARAMS: MovieRequestParams[] = ['s', 'type', 'y', 'r', 'page', 'v']
+    return data;
+  }
 
-    public async getAll(newParams: MovieRequestParams[]): Promise<any> {
-        const params = {
-            fields: [...this.DEFALUT_REQUEST_PARAMS, ...newParams].join(','),
-        };
+  public async getById(id: string): Promise<any> {
+    const params = { ...this.DEFALUT_REQUEST_PARAMS, i: id };
 
-        const { data } = await this.API.get('', {
-            params,
-        });
+    const { data } = await this.API.get("", { params });
 
-        return data;
-    }
+    return data;
+  }
 }
 
-export const countryAPI = new MovieAPI();
+export const movieAPI = new MovieAPI();
