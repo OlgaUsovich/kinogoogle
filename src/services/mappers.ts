fcd6e8e7040367c98090c043644ced53f1267f43
Olga <olga@gmail.com>
2022-08-20 20:59:41 +0300

    feat(api): add movie data mapper

---
+++ src/services/mappers.ts
@@ -0,0 +1,21 @@
+import { IMovie } from "../pages/Home/Home";
+
+export const transformMovie = (movie: any): IMovie => {
+  return {
+    imdbID: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    released: movie.Released,
+    runtime: movie.Runtime,
+    genre: movie.Genre.split(", "),
+    director: movie.Director,
+    writer: movie.Writer,
+    actors: movie.Actors.split(", "),
+    plot: movie.Plot,
+    country: movie.Country,
+    poster: movie.Poster,
+    imdbRating: movie.imdbRating,
+    type: movie.Type,
+    boxOffice: movie.BoxOffice,
+  };
+};
