e5ef4ac7134e0a515d97d60a45cf646c9e6017a2
Olga <olga@gmail.com>
2022-08-20 20:51:42 +0300

    feat(styles): add styles for root div

--- src/ui/globalStyles.ts
+++ src/ui/globalStyles.ts
@@ -1,4 +1,7 @@
 import { createGlobalStyle } from "styled-components";
+import { COLOR } from "./colors";
+import { MEDIA } from "./media";
+import { PADDING } from "./paddings";
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -8,12 +11,28 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  html, body, #root {
-    height: 100%;
+  html, body {
+    min-height: 100%;
   }
 
   body {
     font-family: 'Noto Sans', 'sans-serif';
     background-color: #EAF2F2;
   }
+
+  .root {
+    display: flex;
+    justify-content: center;
+    gap: 146px;
+    background-color: ${COLOR.BLACK};
+    padding: ${PADDING.LG};
+
+    @media (min-width: ${MEDIA.MD}) {
+        ${PADDING.MD}
+    }
+
+    @media (min-width: ${MEDIA.SM}) {
+        ${PADDING.SM}
+    }
+}
 `;
