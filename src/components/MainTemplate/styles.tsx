7c337d53139b7856f38f092df07e0bddee45b7a0
Olga <olya.usovich.17@mail.ru>
2022-08-22 21:35:55 +0300

    fix(styles): move class root styles to wrapper

---
+++ src/components/MainTemplate/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { COLOR } from "../../ui";
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 146px;
+    height: 100vh;
+    background-color: ${COLOR.BLACK};
+`
--- src/ui/globalStyles.ts
+++ src/ui/globalStyles.ts
@@ -9,19 +9,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  html, body {
-    min-height: 100%;
-  }
-
   body {
     font-family: 'Noto Sans', 'sans-serif';
-    background-color: #EAF2F2;
   }
-
-  .root {
-    display: flex;
-    justify-content: center;
-    gap: 146px;
-    background-color: ${COLOR.BLACK};
-}
 `;
