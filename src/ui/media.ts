2dd0e7999696b031207427fdf7f5b77d3233a08e
Olga <olga@gmail.com>
2022-08-28 14:40:44 +0300

    fix(styling): fix media breakpoints

--- src/ui/media.ts
+++ src/ui/media.ts
@@ -1,5 +1,6 @@
 export enum MEDIA {
-  SM = "@media (max-width: 320px)",
+  SM = "@media (max-width: 568px)",
   MD = "@media (max-width: 768px)",
+  MDLG = "@media (max-width: 1024px)",
   LG = "@media (max-width: 1920px)",
 }
