63b47bbd0842207964e336dd698b1010b62d2970
Olga Usovich <olga.usovich.17@mail.ru>
2022-09-09 23:13:46 +0300

    fix(store): fix serializable error

--- src/store/store.ts
+++ src/store/store.ts
@@ -36,6 +36,7 @@ export const store = configureStore({
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        serializableCheck: false,
       },
     }),
 });
