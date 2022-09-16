a6cf537118836048ff9167fc4cffb8e98997a502
Olga Usovich <olya.usovich.17@mail.ru>
2022-09-16 17:58:27 +0300

    fix(log_out): fix logout redirect

--- src/pages/LogOut/LogOut.tsx
+++ src/pages/LogOut/LogOut.tsx
@@ -1,10 +1,20 @@
-import { Navigate } from "react-router-dom";
+import { Spinner } from "components";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { ROUTE } from "../../routers";
 import { logOutUser } from "../../store/features/userSlice";
 import { useAppDispatch } from "../../store/hooks";
 
 export const LogOut = () => {
   const dispatch = useAppDispatch();
-  dispatch(logOutUser());
-  return <Navigate to={ROUTE.HOME} />;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(logOutUser())
+      .then(() => {
+        navigate(ROUTE.HOME, { replace: true });
+      });
+  }, [dispatch, navigate]);
+
+  return <Spinner />;
 };
