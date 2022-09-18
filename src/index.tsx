8c96d329ab0b769f4a1ecbe20aeb12256814645b
Olga Usovich <olga.usovich.17@mail.ru>
2022-09-18 13:52:46 +0300

    build(whole_project): install gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "App";
+import App from "./App";
 import { GlobalStyle } from "ui";
 import { persistor, store } from "store";
 
